661d834630f42b3ee41e3585f5defdb171b70235
fazt <fazt@faztweb.com>
2020-04-26 22:40:27 -0500

    port changed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 
 const app = express();
 
+// settings
+app.set('port', process.env.PORT || 3000);
+
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'));
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const {createConnection} = require('./database');
 
 createConnection();
-app.listen(3000);
-console.log('Server on port', 3000);
+app.listen(app.get('port'));
+console.log('Server on port', app.get('port'));
